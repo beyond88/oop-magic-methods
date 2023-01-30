6b5347bd0ca85c64f02004eb69ad86255f873a43
beyond88 <muhin.cse.diu@gmail.com>
2023-01-31 00:17:54 +0600

    __debuginfo() magic method

---
+++ DebugInfo.php
@@ -0,0 +1,18 @@
+<?php
+class DebugInfo{
+
+    public $employee;
+    private $email;
+    private $eno;
+
+    public function __debugInfo()
+    {
+        return [ 'Employee_Name' => $this->employee ];
+    }
+
+}
+ 
+$obj4 = new DebugInfo();
+var_dump($obj4);
+
+//object(DebugInfo)#1 (1) { ["Employee_Name"]=> NULL }
