71a4d7fcaff62dd2e547710e460ba20c26ca35f0
Mohiuddin Abdul Kader <muhin.cse.diu@gmail.com>
2023-01-31 15:43:55 +0600

    Update Set.php

--- Set.php
+++ Set.php
@@ -16,3 +16,6 @@ $objStudent = new Set();
 // __set() called 
 $objStudent->phone = '0491 570 156';
 
+//we’re trying to set the phone property, which is non-existent. And thus, the __set() method is called. 
+//The first argument of the __set() method is the name of the property which is being accessed, and the second argument is the value we’re trying to set.
+
