de41d838ae81568634d878873cc3ce3e2cf83779
Mohiuddin Abdul Kader <muhin.cse.diu@gmail.com>
2023-01-31 15:41:46 +0600

    Update set.php

---
+++ Set.php
@@ -0,0 +1,15 @@
+<?php
+
+class Set {
+    private $data = array();
+    public function __set($name, $value) 
+    {
+        $this->data[$name] = $value;
+    }
+}
+
+$objStudent = new Set();
+
+// __set() called 
+$objStudent->phone = '0491 570 156';
+
