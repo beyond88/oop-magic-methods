6ef5b6b89054054fe034b9f6bc6e587d3dd888a5
Mohiuddin Abdul Kader <muhin.cse.diu@gmail.com>
2023-01-31 16:20:43 +0600

    Added unset class

---
+++ Unset.php
@@ -0,0 +1,24 @@
+<?php
+
+class Unset {
+
+  private $dat = array();
+  
+	public function __isset( $n ){
+    return isset( $this->dat[$n] );
+  } 
+  
+  public function __unset( $n ){
+		echo "__unset() magic method invoked.";
+		unset( $this->dat[$n] );      
+  }
+  
+}
+ 
+$obj2 = new Unset();
+$obj2->a = 62;
+
+if ( isset( $obj2->a ) )
+    unset( $obj2->a );
+echo "Destroyed obj2 ->a ";
+
