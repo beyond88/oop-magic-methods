c90b275e2c3227d5d678ab27d92e4334001b4e65
beyond88 <muhin.cse.diu@gmail.com>
2023-01-31 11:00:05 +0600

    minor refactore code of set state

--- SetState.php
+++ SetState.php
@@ -1,12 +1,16 @@
 <?php
+
 class SetState {
+
     public $name;
     private $email;   
+
     public function __construct($name, $email) 
     {
         $this->name = $name;
         $this->email = $email;
     }
+
     public static function __set_state(array $array) 
     {
         $obj = new SetState;
@@ -14,8 +18,11 @@ class SetState {
         $obj->email = $array['email'];
         return $obj;
     }
+    
 }
+
 $objStudent = new SetState('John','john@example.com');
 var_export($objStudent);
+
 // Output: Student::__set_state(array( 'name' => 'John', 'email' => 'john@example.com', )) 
 ?>
