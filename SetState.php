17fcd2c82fdc29f3ed912eea5adc8139d20832df
beyond88 <muhin.cse.diu@gmail.com>
2023-01-31 10:59:08 +0600

    updated code for set state

---
+++ SetState.php
@@ -0,0 +1,21 @@
+<?php
+class SetState {
+    public $name;
+    private $email;   
+    public function __construct($name, $email) 
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+    public static function __set_state(array $array) 
+    {
+        $obj = new SetState;
+        $obj->name = $array['name'];
+        $obj->email = $array['email'];
+        return $obj;
+    }
+}
+$objStudent = new SetState('John','john@example.com');
+var_export($objStudent);
+// Output: Student::__set_state(array( 'name' => 'John', 'email' => 'john@example.com', )) 
+?>
